cccfc9a0b5e9e70229e96c022e6a720557628c14
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-04-22 13:26:47 +0330

    Update index.php

--- index.php
+++ index.php
@@ -144,3 +144,36 @@ if (isset($_POST['submit'])) {
                   ?>
 
                     <tr>
+                      <td> <?php if ($format->qualityLabel) echo $format->qualityLabel;
+                            else echo "Unknown"; ?>
+                      </td>
+                      <td><?php if ($format->mimeType) echo explode(";", explode("/", $format->mimeType)[1])[0];
+                          else echo "Unknown"; ?>
+                      </td>
+                      <td><a href="./scripts/download_video.php?link=<?php echo urlencode($url) ?>&title=<?php echo urlencode($title) ?>&type=<?php if ($format->mimeType) echo explode(";", explode("/", $format->mimeType)[1])[0];
+                                                                                                                                              else echo "mp4"; ?>" id="download_btn"><i class='bx bxs-download'></i>&nbsp;Download</a></td>
+                    </tr>
+
+                  <?php } ?>
+
+                </table>
+
+              </div>
+        </div>
+
+    <?php }
+          }
+        } else { ?>
+    <div class="instruction_box">
+      <label><i class='bx bx-video-off'></i></label>
+      <h3>Unable to fetch video info.</h3>
+      <p>make sure url is valid.</p>
+    </div>
+  <?php } ?>
+    </div>
+  </div>
+  <!--=========== JS ===========-->
+  <script src="js/color_palette.js"></script>
+</body>
+
+</html>
