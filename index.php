8a62c5a9442363454c1136e90bff888122a8d66d
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-04-22 13:25:54 +0330

    Update index.php

--- index.php
+++ index.php
@@ -11,3 +11,27 @@ if (isset($_POST['submit'])) {
     if ($isVideoIdValid == "1") {
       $video_id =  $match[1];
 
+      // getting video information
+      $video = json_decode(GetVideoInfo($video_id));
+      $isvalid = $video->playabilityStatus->status;
+
+      $formats = $video->streamingData->formats;
+      $thumbnails = $video->videoDetails->thumbnail->thumbnails;
+      $title = $video->videoDetails->title;
+      $short_description = $video->videoDetails->shortDescription;
+      $channel_id = $video->videoDetails->channelId;
+      $channel_name = $video->videoDetails->author;
+      $views = $video->videoDetails->viewCount;
+      $video_duration_in_seconds = $video->videoDetails->lengthSeconds;
+      $thumbnail = end($thumbnails)->url;
+
+      // seconds to minutes&hours
+      $hours = floor($video_duration_in_seconds / 3600);
+      $minutes = floor(($video_duration_in_seconds / 60) % 60);
+      $seconds = $video_duration_in_seconds % 60;
+    }
+  }
+}
+
+?>
+
